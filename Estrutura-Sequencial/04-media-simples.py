f36103ea969dfd782bf7057defe2476819612963
lanhybrid <lan.mascarin@gmail.com>
2021-05-16 12:18:46 -0300

    exercicios inicio

---
+++ Estrutura-Sequencial/04-media-simples.py
@@ -0,0 +1,6 @@
+n1 = float(input('Nota do 1º Bimestre: '))
+n2 = float(input('Nota do 2º Bimestre: '))
+n3 = float(input('Nota do 3º Bimestre: '))
+n4 = float(input('Nota do 4º Bimestre: '))
+media = (n1 + n2 + n3 + n4)/4
+print(f'A média final é {media:.2f}')
